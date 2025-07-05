f1ea9e7c75e9dfaa328203f992c8627df452d78a
Chetan Singh <chetan@candidate.live>
2025-07-05 19:57:49 +0530

    added python-in-shell

---
+++ python-in-shell/hello_chai.py
@@ -0,0 +1,13 @@
+def chai(n):
+    print(n)
+
+chai_one = "Ginger tea"
+chai_two ="masala chai"
+
+## we can import this in other file as:
+## from hello_chai import chai
+## we can also import attributes and functions etc
+
+
+## call them as: hello_chai.chai() 
+## hello_chai.chai_one
